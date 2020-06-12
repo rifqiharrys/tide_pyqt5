846d97c19acf0ce6f7a50383f5466838138633d9
rifqiharrys <rifqi.m.h@gmail.com>
2020-06-12 20:17:37 +0700

    add widget

--- tide_merge.py
+++ tide_merge.py
@@ -30,6 +30,59 @@ class MergeData(QWidget):
 		self.setWindowTitle('Merge Data GUI')
 		self.setWindowIcon(QIcon('wave-pngrepo-com.png'))
 
+		fileLocButton = QPushButton('Open File Location')
+		# fileLocButton.clicked.connect(self.inputPathDialog)
+		# plotObsButton = QPushButton('Plot Observation Data')
+		# plotObsButton.clicked.connect(self.plotLoad)
+		self.locLineForm = QLineEdit()
+
+		headerLineLabel = QLabel('Header Starting Line:')
+		self.headerLineSB = QSpinBox()
+		self.headerLineSB.setMinimum(1)
+
+		dataLineLabel = QLabel('Data Starting Line:')
+		self.dataLineSB = QSpinBox()
+		self.dataLineSB.setMinimum(1)
+
+		timeHeaderLabel = QLabel('Time Header:')
+		self.timeHeaderLineForm = QLineEdit()
+
+		depthHeaderLabel = QLabel('Depth Header:')
+		self.depthHeaderLineForm = QLineEdit()
+
+		dayFirstLabel = QLabel('Day First:')
+		self.dayFirstCB = QComboBox()
+		self.dayFirstCB.addItems(['True', 'False'])
+
+		sepLabel = QLabel('Separator:')
+		self.sepCB = QComboBox()
+		self.sepCB.addItems(['Tab', 'Comma', 'Space', 'Semicolon'])
+
+		grid = QGridLayout()
+		vbox = QVBoxLayout()
+
+		grid.addWidget(fileLocButton, 1, 1, 1, 1)
+		grid.addWidget(self.locLineForm, 1, 2, 1, 3)
+
+		grid.addWidget(headerLineLabel, 2, 1, 1, 1)
+		grid.addWidget(self.headerLineSB, 2, 2, 1, 1)
+		grid.addWidget(dataLineLabel, 2, 3, 1, 1)
+		grid.addWidget(self.dataLineSB, 2, 4, 1, 1)
+
+		grid.addWidget(timeHeaderLabel, 3, 1, 1, 1)
+		grid.addWidget(self.timeHeaderLineForm, 3, 2, 1, 1)
+		grid.addWidget(depthHeaderLabel, 3, 3, 1, 1)
+		grid.addWidget(self.depthHeaderLineForm, 3, 4, 1, 1)
+
+		grid.addWidget(dayFirstLabel, 4, 1, 1, 1)
+		grid.addWidget(self.dayFirstCB, 4, 2, 1, 1)
+		grid.addWidget(sepLabel, 4, 3, 1, 1)
+		grid.addWidget(self.sepCB, 4, 4, 1, 1)
+
+		vbox.addStretch(1)
+		grid.addLayout(vbox, 20, 1)
+		self.setLayout(grid)
+
 
 def entry():
 

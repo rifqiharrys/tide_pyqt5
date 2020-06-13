ca2504e39e8b1cb8e436fe7795b7ec2b51cb8761
rifqiharrys <rifqi.m.h@gmail.com>
2020-06-13 21:06:57 +0700

    add merge function

--- tide_merge.py
+++ tide_merge.py
@@ -44,8 +44,12 @@ class MergeData(QWidget):
 		self.dataLineSB = QSpinBox()
 		self.dataLineSB.setMinimum(1)
 
-		timeHeaderLabel = QLabel('Time Header:')
-		self.timeHeaderLineForm = QLineEdit()
+		indexLabel = QLabel('Index Name:')
+		self.indexLineForm = QLineEdit()
+
+		sepLabel = QLabel('Separator:')
+		self.sepCB = QComboBox()
+		self.sepCB.addItems(['Tab', 'Comma', 'Space', 'Semicolon'])
 
 		depthHeaderLabel = QLabel('Depth Header:')
 		self.depthHeaderLineForm = QLineEdit()
@@ -53,10 +57,6 @@ class MergeData(QWidget):
 		dayFirstLabel = QLabel('Day First:')
 		self.dayFirstCB = QComboBox()
 		self.dayFirstCB.addItems(['True', 'False'])
-
-		sepLabel = QLabel('Separator:')
-		self.sepCB = QComboBox()
-		self.sepCB.addItems(['Tab', 'Comma', 'Space', 'Semicolon'])
 
 		saveLocButton = QPushButton('Save File Location')
 		saveLocButton.clicked.connect(self.savePathDialog)
@@ -75,20 +75,15 @@ class MergeData(QWidget):
 		grid.addWidget(dataLineLabel, 2, 3, 1, 1)
 		grid.addWidget(self.dataLineSB, 2, 4, 1, 1)
 
-		grid.addWidget(timeHeaderLabel, 3, 1, 1, 1)
-		grid.addWidget(self.timeHeaderLineForm, 3, 2, 1, 1)
-		grid.addWidget(depthHeaderLabel, 3, 3, 1, 1)
-		grid.addWidget(self.depthHeaderLineForm, 3, 4, 1, 1)
+		grid.addWidget(sepLabel, 3, 1, 1, 1)
+		grid.addWidget(self.sepCB, 3, 2, 1, 1)
+		grid.addWidget(indexLabel, 3, 3, 1, 1)
+		grid.addWidget(self.indexLineForm, 3, 4, 1, 1)
 
-		grid.addWidget(dayFirstLabel, 4, 1, 1, 1)
-		grid.addWidget(self.dayFirstCB, 4, 2, 1, 1)
-		grid.addWidget(sepLabel, 4, 3, 1, 1)
-		grid.addWidget(self.sepCB, 4, 4, 1, 1)
+		grid.addWidget(saveLocButton, 4, 1, 1, 1)
+		grid.addWidget(self.saveLocLineForm, 4, 2, 1, 3)
 
-		grid.addWidget(saveLocButton, 5, 1, 1, 1)
-		grid.addWidget(self.saveLocLineForm, 5, 2, 1, 3)
-
-		grid.addWidget(self.dataFrame, 6, 1, 4, 4)
+		grid.addWidget(self.dataFrame, 5, 1, 4, 4)
 
 		vbox.addStretch(1)
 		grid.addLayout(vbox, 20, 1)
@@ -114,6 +109,39 @@ class MergeData(QWidget):
 		fname = QFileDialog.getSaveFileName(self, 'Save File', home_dir, 'Text files (*.txt)')
 		filePath = (str(Path(fname[0])))
 		self.saveLocLineForm.setText(filePath)
+
+
+	def inputDict(self):
+
+		location = self.locLineForm.text()
+		head = self.headerLineSB.value() - 1
+		start_data = self.dataLineSB.value() - 1
+		sepDict = {'Tab': '\t', 'Comma': ',', 'Space': ' ', 'Semicolon': ';'}
+		sepSelect = sepDict[self.sepCB.currentText()]
+		index = self.indexLineForm.text()
+
+		input_dict = {'location':location, 'head':head, 'start':start_data, 'separator':sepSelect, 'index':index}
+
+		return input_dict
+
+
+	def merge(self):
+
+		input_dict = self.inputDict()
+		files = input_dict['location'] + '*.[Tt][Xx][Tt]'
+		txtlist = glob.glob(files)
+
+		dummy = []
+
+		for txt in txtlist:
+			raw = pd.read_csv(txt, sep=input_dict['separator'], header=input_dict['head'], index_col=input_dict['index'])
+			raw = raw.iloc[input_dict['start']:, 0:]
+
+			dummy.append(raw)
+		
+		merged = pd.concat(dummy, sort=True)
+
+		return merged.sort_index()
 
 
 
